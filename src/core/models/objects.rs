80300e76fe041997082e18a099c3bce4b3718620
deyanslavkov <deyanslavkov@gmail.com>
2025-12-13 16:23:28 +0200

    New objects

--- src/core/models/objects.rs
+++ src/core/models/objects.rs
@@ -22,3 +22,30 @@ pub struct StagedFile {
     pub mode: u32,
     pub size: u64,
 }
+
+#[derive(Debug, Clone)]
+pub struct Commit {
+    pub hash: [u8; 32],
+    pub parent_hash: Option<[u8; 32]>,
+    pub merge_parent_hash: Option<[u8; 32]>,
+    pub tree_hash: [u8; 32],
+    pub message: String,
+    pub author_id: i64,
+    pub timestamp: i64,
+}
+
+#[derive(Debug, Clone)]
+pub struct TreeEntry {
+    pub parent_hash: [u8; 32],
+    pub name: String,
+    pub object_hash: [u8; 32],
+    pub mode: u32,
+    pub is_dir: bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct Author {
+    pub id: i64,
+    pub name: String,
+    pub email: String,
+}
